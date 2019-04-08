63f0fa06760cd7cb719a9d7bd4bcb4e69122660b
Drew Shamrock <drewshamrock@gmail.com>
2019-04-08 09:21:24 -0700

    Add basic routing for homepage, category and item pages

--- catalog.py
+++ catalog.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def homepage():
+    return render_template('index.html')
 
+@app.route('/category/')
+def category_page():
+    return render_template('category.html')
+
+@app.route('/category/item/')
+def item_page():
+    return render_template('item.html')
+    
 if __name__ == '__main__':
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
