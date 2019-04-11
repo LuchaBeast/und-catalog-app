09d2ca85e1cc1fb868b5b987fb2d9449ce5bf708
Drew Shamrock <drewshamrock@gmail.com>
2019-04-11 10:33:20 -0700

    Add pycache to gitignore and add database to catalog app

--- catalog.py
+++ catalog.py
@@ -1,6 +1,16 @@
 from flask import Flask
 from flask import render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, Item
+
 app = Flask(__name__)
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
 
 @app.route('/')
 def homepage():
