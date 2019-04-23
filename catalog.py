65be278241e1b09e0fa1946c9038b769c1dc1a7d
Drew Shamrock <drewshamrock@gmail.com>
2019-04-23 11:12:45 -0700

    Fix redirect in newItem function

--- catalog.py
+++ catalog.py
@@ -45,7 +45,7 @@ def newItem(category_id):
                        category_id=category_id)
         session.add(newItem)
         session.commit()
-        return redirect(url_for('categoryPage', category_id=category.id))
+        return redirect(url_for('category', category_id=category_id))
     else:
         return render_template('new-item.html',
                                all_categories=all_categories,
